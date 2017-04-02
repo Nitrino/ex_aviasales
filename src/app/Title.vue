7330b05dd9019dfadcefff39006473db3676ddf3
Ainur Timerbaev <nur.php@gmail.com>
2017-04-03 01:46:23 +0700

    fix(title): add google translate link

--- src/app/Title.vue
+++ src/app/Title.vue
@@ -5,7 +5,7 @@
     </div>
     <div class="what_happened">
       <img class="what_happened__emoji" src="images/what_happened.svg">
-      What&rsquo;s happen? Why are we&nbsp;looking for new job? </br><a href="https://vc.ru/n/aviasales-cut">Here is</a>&nbsp;the answer. And here is&nbsp;our portfolios.
+      What&rsquo;s happen? Why are we&nbsp;looking for new job? </br><a target="_blank" href="https://vc.ru/n/aviasales-cut">Here is (RU)</a> <a target="_blank" href="https://translate.google.com/translate?sl=ru&tl=en&js=y&prev=_t&hl=ru&ie=UTF-8&u=https%3A%2F%2Fvc.ru%2Fn%2Faviasales-cut&edit-text=">(google translate)</a>&nbsp;the answer. And here is&nbsp;our portfolios.
     </div>
   </div>
 </template>
