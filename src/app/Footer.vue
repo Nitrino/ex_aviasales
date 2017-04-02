0cd126a308741e0fc6e3a8b84a252e91c48a6cf1
Petr Stepchenko <nitrinodev@gmail.com>
2017-04-02 21:59:10 +0700

    add footer and socials

--- src/app/Footer.vue
+++ src/app/Footer.vue
@@ -1,5 +1,18 @@
 <template>
   <footer class="footer">
+    <div class="footer__left">
+      <img src="images/footer_left.png" alt="">
+    </div>
+    <div class="footer__social">
+      <div class="footer__social__text">
+        Тут&nbsp;бы текст аля &laquo;Сделай доброе дело&nbsp;&mdash;</br>
+        расскажи друзьям&raquo; и&nbsp;все такое
+      </div>
+      <div class="footer__social__buttons sharethis-inline-share-buttons"></div>
+    </div>
+    <div class="footer__right">
+      <img src="images/footer_right.png" alt="">
+    </div>
   </footer>
 </template>
 
