d1349f7d8333eb7fb6cbd3357a60182c9ea39e42
Ainur Timerbaev <nur.php@gmail.com>
2017-04-02 20:51:34 +0700

    feat(employees): add scroll to text

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue';
 import Main from './app/Main.vue';
 
 import './index.scss';
+import vueSmoothScroll from 'vue-smooth-scroll';
+Vue.use(vueSmoothScroll);
 
 export default new Vue({
   el: '#root',
