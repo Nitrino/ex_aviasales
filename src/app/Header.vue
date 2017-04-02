4d5988e0e8227f62b5b4fce284f9c3687d05043d
Ainur Timerbaev <nur.php@gmail.com>
2017-04-02 22:57:27 +0700

    fix(header): remove lang chooser

--- src/app/Header.vue
+++ src/app/Header.vue
@@ -4,10 +4,6 @@
       <img src="images/header_left.png">
     </div>
     <div class="header__block -middle">
-      <div class="header__lang">
-        <a href="#">RU</a>
-        <a href="#">EN</a>
-      </div>
       <div class="header__logo">
         <img src="images/logo.svg">
         <img class="header__logo__text" src="images/x-aviasales.svg">
